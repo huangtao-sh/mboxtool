0dbc6f2eec6337f7b96ede70d4aa49b85dc9e32a
=huangtao <=huangtao@bankcomm.com>
2014-09-25 09:34:49 +0800

    test

--- setup.py
+++ setup.py
@@ -2,5 +2,10 @@
 from distutils.core import setup
 setup(
         name='mboxtool',
+        version='0.0.1',
         author='Huang tao',
+        author_email='huangtao.jh@gmail.com',
+        platforms='any',
+        description='mbox import&export tools',
+        scripts=['mboxtool.py'],
         )
